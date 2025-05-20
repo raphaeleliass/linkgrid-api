b2cbfa6345304ff1d99715ed32488c29a4078596
Raphael Elias <raphaeleliass@outlook.com>
2025-05-20 15:10:49 -0300

    feat: add detail user type inference to user types module

--- src/modules/user/user.types.ts
+++ src/modules/user/user.types.ts
@@ -1,8 +1,10 @@
 import { z } from "zod";
-import { authUserSchema, createUserSchema, userSchema } from "./user.schemas";
+import { authUserSchema, createUserSchema, detailUserSchema, userSchema } from "./user.schemas";
 
 export type UserTypes = z.infer<typeof userSchema>;
 
 export type CreateUserTypes = z.infer<typeof createUserSchema>;
 
 export type AuthUserTypes = z.infer<typeof authUserSchema>;
+
+export type DetailUserTypes = z.infer<typeof detailUserSchema>
